6a6cb6c26422405e3f3b0f28a0c649c8d9430f2d
Saron M Demeke <demek004@umn.edu>
2023-01-30 23:38:43 -0600

    done through line 8

--- R/week3.R
+++ R/week3.R
@@ -1,3 +1,8 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#Data Import and Cleaning
+raw_df <- read.csv("../data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %T")
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %T")
+clean_df <- raw_df[raw_df$timeStart >= as.POSIXct("2017-07-01 00:00:00"),]
