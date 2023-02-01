37b84071f8363c72cfc1efeaf20f7a90643f5b3e
Saron M Demeke <demek004@umn.edu>
2023-02-01 11:46:07 -0600

    done through line 13

--- R/week3.R
+++ R/week3.R
@@ -6,3 +6,8 @@ raw_df <- read.csv("../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart, format = "%Y-%m-%d %T")
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd, format = "%Y-%m-%d %T")
 clean_df <- raw_df[raw_df$timeStart >= as.POSIXct("2017-07-01 00:00:00"),]
+clean_df <- clean_df[clean_df$q6 == 1,]
+
+#Analysis
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs")
+hist(as.numeric(clean_df$timeSpent))
