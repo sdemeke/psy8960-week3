3ec62f885fe0fe5c0bb0fca926913b468fc0aa2b
Saron M Demeke <demek004@umn.edu>
2023-02-01 13:09:37 -0600

    revised line 16 using apply()

--- R/week3.R
+++ R/week3.R
@@ -13,6 +13,6 @@ clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "se
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
-nrow(clean_df[( (clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3) ),])
+sum(apply(clean_df,1,function(x){( (as.numeric(x[5]) >= as.numeric(x[6])) & (as.numeric(x[6]) != as.numeric(x[7])))} ))
 for(i in length(frequency_tables_list)) {
   barplot(frequency_tables_list[[i]]) }
